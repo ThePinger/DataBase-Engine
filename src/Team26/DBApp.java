89b194a8860598118fc6528dc984bdf1d1bef298
ThePinger <omaremadsabry@gmail.com>
2018-02-18 12:48:05 +0200

    [+] Added DBApp constructor , loadDB(), save()

--- src/Team26/DBApp.java
+++ src/Team26/DBApp.java
@@ -1,6 +1,47 @@
 package Team26;
 
-public class DBApp 
-{
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.TreeSet;
 
+public class DBApp implements Serializable
+{
+	private String curDB;
+	private String curDBFilePath;
+	private String dataBasesDataFilePath = "/data/DataBases/DataBases.class";
+	private TreeSet<String> dataBases;
+	
+	public DBApp() throws FileNotFoundException, IOException, ClassNotFoundException
+	{
+		loadDB();	
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void loadDB() throws FileNotFoundException, IOException, ClassNotFoundException
+	{
+		File savedDataBases = new File(this.dataBasesDataFilePath);
+		if(savedDataBases.exists())
+		{
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(savedDataBases));
+			this.dataBases = (TreeSet<String>) in.readObject();
+		}
+		else
+		{
+			this.dataBases = new TreeSet<String>();
+			save();
+		}
+	}
+	
+	public void save() throws FileNotFoundException, IOException
+	{
+		File dataBasesInfo = new File(this.dataBasesDataFilePath);
+		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(dataBasesInfo));
+		out.writeObject(this.dataBases);
+	}
 }
