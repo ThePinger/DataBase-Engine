82c015f08a4e668d17b147aa68eee0afa1a3636a
ThePinger <omaremadsabry@gmail.com>
2018-02-20 09:54:17 +0200

    [+] Added Table Constructor, createPath()

--- src/Team26/Table.java
+++ src/Team26/Table.java
@@ -1,20 +1,31 @@
 package Team26;
 
+import java.io.File;
 import java.io.Serializable;
 import java.util.Hashtable;
 
 public class Table implements Serializable 
 {
-	private String tableName;
-	private Hashtable<String, String> tableFormat;
-	private String key;
 	private int numberOfPages;
+	private String key;
+	private String tableName;
+	private String filePath = "data/DataBases/";
+	private Hashtable<String, String> tableFormat;
 	
-	public Table(String name, Hashtable<String, String> format, String key)
+	public Table(String name, Hashtable<String, String> format, String key, String dbName)
 	{
 		this.tableName = name;
 		this.tableFormat = format;
 		tableFormat.put("TouchDate", "java.util.Date");
 		this.key = key;
+		this.filePath += dbName + "/" + this.tableName + "/";
+		createPath();
+		this.numberOfPages = 1;
+	}
+	
+	public void createPath()
+	{
+		File path = new File(this.filePath);
+		path.mkdirs();
 	}
 }
