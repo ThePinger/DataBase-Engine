c23559be26745336ecb4e47c44c49685b886fbc2
ThePinger <omaremadsabry@gmail.com>
2018-02-20 10:56:50 +0200

    [+] Added saveMetaData() in Table.java

--- src/Team26/Table.java
+++ src/Team26/Table.java
@@ -1,8 +1,15 @@
 package Team26;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Iterator;
+
 
 public class Table implements Serializable 
 {
@@ -12,7 +19,7 @@ public class Table implements Serializable
 	private String filePath = "data/DataBases/";
 	private Hashtable<String, String> tableFormat;
 	
-	public Table(String name, Hashtable<String, String> format, String key, String dbName)
+	public Table(String name, Hashtable<String, String> format, String key, String dbName) throws FileNotFoundException
 	{
 		this.tableName = name;
 		this.tableFormat = format;
@@ -20,6 +27,7 @@ public class Table implements Serializable
 		this.key = key;
 		this.filePath += dbName + "/" + this.tableName + "/";
 		createPath();
+		saveMetaData();
 		this.numberOfPages = 1;
 	}
 	
@@ -27,5 +35,24 @@ public class Table implements Serializable
 	{
 		File path = new File(this.filePath);
 		path.mkdirs();
+	}
+	
+	public void saveMetaData() throws FileNotFoundException
+	{
+		File path = new File(this.filePath + "metadata.csv");
+		PrintWriter pw = new PrintWriter(path);
+		Enumeration<String> colName = this.tableFormat.keys();
+		Iterator<String> colType =  this.tableFormat.values().iterator();
+		while(colType.hasNext())
+		{
+			String colname = colName.nextElement();
+			if(colname.equals(this.key))
+				pw.println(this.tableName + "," + colname + "," + colType.next() + "," + true + "," + false);
+			else
+				pw.println(this.tableName + "," + colname + "," + colType.next() + "," + false + "," + false);
+		}
+		
+		pw.flush();
+		pw.close();
 	}
 }
