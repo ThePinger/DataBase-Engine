ee86e1bdb0e02787079e322f1ac9f86bf79daf7e
Mohamed Nawar <mohamedynawar@gmail.com>
2018-02-20 12:13:49 +0200

    Added method createRecord
    This will be used when inserting a record into the table.

--- src/Team26/Table.java
+++ src/Team26/Table.java
@@ -1,8 +1,17 @@
 package Team26;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.TreeSet;
 
 public class Table implements Serializable 
 {
@@ -28,4 +37,34 @@ public class Table implements Serializable
 		File path = new File(this.filePath);
 		path.mkdirs();
 	}
+	
+	public Record createRecord(Hashtable<String, Object> htblColNameValue) throws DBAppException
+	{
+		ArrayList<Object> recordData = new ArrayList<Object>();
+		Enumeration<String> columnNames = (Enumeration<String>) tableFormat.keys();
+		int keyIndex = 0;
+		String keyType = null;
+		
+		while(columnNames.hasMoreElements()) // Checks if all columns in the insertion are found in the table.
+		{
+			String column = columnNames.nextElement();
+			
+			if(!htblColNameValue.containsKey(column))
+			{
+				throw new DBAppException("Error: your insertion does not match the desired table's format.");
+			}
+			
+			recordData.add(htblColNameValue.get(column));
+			
+			if(column.equals(key))
+			{
+				keyIndex = recordData.size() - 1;
+				keyType  = tableFormat.get(column);
+			}
+		}
+		
+		Record insertion = new Record(recordData, keyType, keyIndex);
+		return insertion;
+	}
+	
 }
