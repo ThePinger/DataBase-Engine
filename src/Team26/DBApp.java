be01793c19167fac487e5db47af3c1c6c4d0379d
Mohamed Nawar <mohamedynawar@gmail.com>
2018-02-20 15:56:14 +0200

    Changed the location of the config file.

--- src/Team26/DBApp.java
+++ src/Team26/DBApp.java
@@ -122,7 +122,7 @@ public class DBApp implements Serializable
 	
 	public void readConfigFile() throws IOException
 	{
-		FileInputStream in = new FileInputStream("src/Config/DBApp.config");
+		FileInputStream in = new FileInputStream("config/DBApp.config");
 		dataBaseProperties = new Properties();
 		dataBaseProperties.load(in);
 		in.close();
